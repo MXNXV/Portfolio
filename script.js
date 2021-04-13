51d15b3e0f66e0abe84a54a1f335e054a1530a44
Manav <manavmandalpune@gmail.com>
2021-04-13 13:13:35 +0530

    Updated skills section

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["AI Enthusiast", "Developer", "Coder", "Coordinator"],
+        strings: ["ML Enthusiast", "Developer", "Coder", "DSC Coordinator"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["AI Enthusiast", "Developer", "Coder", "Coordinator"],
+        strings: ["ML Enthusiast", "Developer", "Coder", "DSC Coordinator"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
